81294e7493185ff3b722cf871d95aea8e49ff000
JRGCast <joao.ralph1@gmail.com>
2021-10-30 00:50:37 -0300

    implemented cors, fecthing is sucessful

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,22 @@
+import { useEffect, useState } from "react";
 const LandingPage = () => {
-  const bodyReq = {
+  const [body, setTheBody] = useState({});
+  const url = "https://desafio-sharenergy-back-end.herokuapp.com/clients";
+  const options = {
+    methods: "GET,PUT,POST,DELETE",
+    mode: "cors",
+    headers: {
+      "Access-Control-Allow-Origin": '*'
+    },
+  };
+  useEffect(() => {
+    fetch(url, options).then(response => response.json()).then(jsoned => setTheBody(jsoned));
+  }, []);
 
-  }
-  const request = ''
   return (
     <div>
       <h1>Iniciando</h1>
+      <p>{ console.log(body) }</p>
     </div>);
 };
 
