e46e4ac412ce1a6873e806dfb30b1a137d757d18
JRGCast <joao.ralph1@gmail.com>
2021-12-11 19:09:41 -0300

    modificado externalUrls

--- src/utils/externalUrls.js
+++ src/utils/externalUrls.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 // Client Urls
-export const clientUrl = process.env.REACT_APP_CLIENT_URL_FULL;
+// export const clientUrl = process.env.REACT_APP_CLIENT_URL_FULL;
+export const clientUrl = 'http://localhost:28060/clients'
 export const clientFindName = `${clientUrl}/byname`;
 export const clientAdd = `${clientUrl}/newclient`;
 export const clientUpdate = `${clientUrl}/updateclient`;
